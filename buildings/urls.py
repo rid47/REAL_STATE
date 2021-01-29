de698d958cfa0ee1495896e4311e04a87cfb5515
Md. Ridwan Mizan <ridwanmizan@gmail.com>
2021-01-29 10:27:53 +0600

    added search

--- buildings/urls.py
+++ buildings/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomeDetailAPIView, HomeListAPIView, ImageView
+from .views import HomeDetailAPIView, HomeListAPIView, ImageView, SearchDataAPIView
 
 urlpatterns = [
     path('', HomeListAPIView.as_view(), name='home'),
     path('<slug>/', HomeDetailAPIView.as_view(), name='home-detail'),
     path('images/<pk>/', ImageView.as_view(), name='images'),
+    path('data/search/', SearchDataAPIView.as_view(), name='search_data'),
     ]
--- buildings/views.py
+++ buildings/views.py
@@ -4,6 +4,7 @@ from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.response import Response
 from .serializers import HomeSerializer, HomeDetailSerializer, ImageFilesSerializer
 from .models import Home, ImageFiles
+from django.db.models import Q
 
 
 class HomeListAPIView(ListAPIView):
@@ -26,4 +27,30 @@ class ImageView(APIView):
         images = home.images.all()
         serilizer = ImageFilesSerializer(images, many=True)
         return Response(serilizer.data, status=status.HTTP_200_OK)
+
+
+class SearchDataAPIView(APIView):
+    # permission_classes = (permissions.AllowAny,)
+    def post(self, request, format=None):
+        # data = self.request.data
+        queryset = Home.objects.filter(is_published=True)
+        try:
+            str = request.data['str']
+            q = (Q(description__icontains=str)) | (Q(title__icontains=str))
+            queryset = queryset.filter(q)
+        except:
+            pass
+        try:
+            price_from = request.data['price_from']
+            queryset = queryset.filter(price__gte=price_from)
+        except:
+            pass
+        try:
+            price_to = request.data['price_to']
+            queryset = queryset.filter(price__lte=price_to)
+        except:
+            pass
+
+        serializer = HomeSerializer(queryset, many=True)
+        return Response(serializer.data)
 
